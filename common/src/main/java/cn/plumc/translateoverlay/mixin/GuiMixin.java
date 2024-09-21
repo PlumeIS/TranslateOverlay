b5b578f73f6bb9a2053df1e716488dad85a6a1e3
BuildTools <unconfigured@null.spigotmc.org>
2024-09-21 19:03:31 +0800

    project rename.

---
+++ common/src/main/java/cn/plumc/translateoverlay/mixin/GuiMixin.java
@@ -0,0 +1,22 @@
+package cn.plumc.translateoverlay.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.util.math.MatrixStack;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import cn.plumc.translateoverlay.translates.ocr.OCRHud;
+
+@Mixin(InGameHud.class)
+public class GuiMixin {
+    @Shadow @Final private MinecraftClient client;
+
+    @Inject(method = "render", at = @At(value = "TAIL"))
+    public void render(MatrixStack matrices, float tickDelta, CallbackInfo ci) {
+        if (client.currentScreen==null) OCRHud.instance.render(matrices, null, null, tickDelta);
+    }
+}
---
+++ fabric/src/main/java/cn/plumc/translateoverlay/fabric/command/argument/FilterArgument.java
@@ -0,0 +1,40 @@
+package cn.plumc.translateoverlay.fabric.command.argument;
+
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import net.fabricmc.fabric.api.client.command.v1.FabricClientCommandSource;
+import net.minecraft.command.CommandSource;
+import cn.plumc.translateoverlay.config.ConfigFile;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.concurrent.CompletableFuture;
+
+public class FilterArgument implements ArgumentType<String> {
+    private static final Collection<String> EXAMPLES = Arrays.asList("AAA", "abc", "foo");
+
+    public static FilterArgument filter(){return new FilterArgument();}
+
+    @Override
+    public String parse(StringReader reader) throws CommandSyntaxException {
+        return reader.readUnquotedString();
+    }
+
+    public static String getFilter(CommandContext<FabricClientCommandSource> context, String argument){
+        return context.getArgument(argument, String.class);
+    }
+
+    @Override
+    public <S> CompletableFuture<Suggestions> listSuggestions(CommandContext<S> context, SuggestionsBuilder builder) {
+        return CommandSource.suggestMatching(ConfigFile.filters, builder);
+    }
+
+    @Override
+    public Collection<String> getExamples() {
+        return EXAMPLES;
+    }
+}
